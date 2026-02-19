87a8b292c6cecfc9b607a1e629ee9512c67bedd4
VECHHA SAI RIDDHI <riddhii0305@gmail.com>
2026-02-19 09:52:11 +0530

    New week

---
+++ week31.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+    int n,x,i;
+    vector<int> v;
+    cout<<"Enter size";
+    cin>>n;
+    for(i=0;i<n;i++)
+    {
+    cout<<"Enter element "<<i+1<<endl;
+    cin>>x;
+    v.push_back(x);
+    }
+    for(auto it=v.begin();it != v.end();it++)
+    {
+        cout<<*it<<" ";
+    }
+    return 1;
+}
