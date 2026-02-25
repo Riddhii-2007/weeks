07a90d69ed28abbc51d8498be021605ece0324c0
VECHHA SAI RIDDHI <riddhii0305@gmail.com>
2026-02-25 13:59:48 +0530

    new week

---
+++ week33.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<map>
+using namespace std;
+int main()
+{
+    map<int ,string> student;
+    student[101]="Riddhi";
+    student[102]="Mouthwika";
+    student[103]="Carry";
+    student[104]="Skylar";
+    for(auto it=student.begin();it != student.end;it++)
+    {
+        cout<<it->first<<" Name :"<<it->second<<endl;
+    }
+    return 0;
+}
