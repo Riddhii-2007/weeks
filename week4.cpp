de77f0410be710341f075177459faef9af317c25
VECHHA SAI RIDDHI <riddhii0305@gmail.com>
2026-03-04 21:56:53 +0530

    just commiting changes

--- week4.cpp
+++ week4.cpp
@@ -1,52 +1,112 @@
-#include<stdio.h>
+#include<iostream>
 using namespace std;
 struct Node{
     int data;
     Node* next;
-}
+};
 class SingleLinkedlist
 {
     private:
     Node* head;
 
     public:
-    //constructor
     SingleLinkedlist()
     {
         head=NULL;
     }
-
-    void deletBegnning()
+    void insterBeginning(int value)
     {
-        if(head==NULL)
-        {
-            cout<<"List is empty";
-            return;
-        }
-        Node* temp==head;
-        head=head->next;
-        delete temp;
-        cout<<"Deleted from beginning successfully"; 
+        Node* newNode=new Node;
+        newNode->data=value;
+        newNode->next=head;
+        head=newNode;
     }
-
-    void deletEnd()
+    void insertEnd(int value)
     {
+        Node* newNode=new Node;
+        newNode->data=value;
+        newNode->next=NULL;
         if(head==NULL)
         {
-            cout<<"List is empty";
-            return;
-        }
-        if(head->next=NULL)
-        {
-            delete head;
-            head=NULL:
-            cout<<"element is deleted at end,list is empty\n";
-            return;
+            head=newNode;
+            cout<<"List is empty,elemnt inserted at beginning";
         }
         Node* temp=head;
-        if(temp->next->next!=NULL)
+        while(temp->next!=NULL)
         {
             temp=temp->next;
         }
+        temp->next=newNode;
     }
+};
+int main()
+{   
+    SingleLinkedlist list;
+    int choice,val,pos;
+    do{
+        cout << "\n===== SINGLY LINKED LIST ADT MENU =====\n";
+        cout << "1. Insert at Beginning\n";
+        cout << "2. Insert at End\n";
+        cout << "3. Insert at Position\n";
+        cout << "4. Delete from Beginning\n";
+        cout << "5. Delete from End\n";
+        cout << "6. Delete at Position\n";
+        cout << "7. Search Element\n";
+        cout << "8. Display List\n";
+        cout << "9. Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+        switch(choice)
+        {
+            case 1:
+                cout << "Enter value: ";
+                cin >> value;
+                list.insertBeginning(value);
+                break;
+
+            case 2:
+                cout << "Enter value: ";
+                cin >> value;
+                list.insertEnd(value);
+                break;
+
+            case 3:
+                cout << "Enter value and position: ";
+                cin >> value >> pos;
+                list.insertAtPosition(value, pos);
+                break;
+
+            case 4:
+                list.deleteBeginning();
+                break;
+
+            case 5:
+                list.deleteEnd();
+                break;
+
+            case 6:
+                cout << "Enter position: ";
+                cin >> pos;
+                list.deleteAtPosition(pos);
+                break;
+
+            case 7:
+                cout << "Enter element to search: ";
+                cin >> value;
+                list.search(value);
+                break;
+
+            case 8:
+                list.display();
+                break;
+
+            case 9:
+                cout << "Exiting Program...\n";
+                break;
+
+            default:
+                cout << "Invalid Choice! Try Again.\n";
+        }
+
+     }
 }
